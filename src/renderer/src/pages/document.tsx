8bfe3665961a6f26257287a7f320432f0f539d47
Vitor Rubim <vitorrubim1@hotmail.com>
2025-12-12 19:47:56 -0300

    feat: pagina de documento

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import { ToC } from "@renderer/components/ToC";
 
 export function DocumentPage() {
   return (
-    <main className="flex-1 flex flex-col items-center justify-center text-rotion-400">
-      Documento
-      <Link to="/">Ir para home</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold uppercase text-xs">
+          Table of contents
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Backend</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Introduction</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <main className="flex-1 flex flex-col items-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </main>
     </main>
   );
 }
