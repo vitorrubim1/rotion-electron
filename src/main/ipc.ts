60db71392d8a4684dc85c0004bff6fffd7ef305b
Vitor Rubim <vitorrubim1@hotmail.com>
2025-12-22 17:26:15 -0300

    feat: conectando com o store

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,17 +1,54 @@
 import { ipcMain } from "electron";
+import { randomUUID } from "node:crypto";
 import { IPC_EVENTS } from "@shared/constants/ipc";
-import { FetchAllDocumentsResponse } from "@shared/types/ipc";
+import * as DTO from "@shared/types/ipc";
+import { store } from "./store";
 
 ipcMain.handle(
   IPC_EVENTS.DOCUMENTS.FETCH_ALL,
-  async (): Promise<FetchAllDocumentsResponse> => {
+  async (): Promise<DTO.FetchAllDocumentsResponse> => {
     return {
-      data: [
-        { id: "1", title: "Untitled", content: "" },
-        { id: "2", title: "Discover", content: "" },
-        { id: "3", title: "Ignite", content: "" },
-        { id: "4", title: "Documentação", content: "" },
-      ],
+      data: Object.values(store.get("documents")),
     };
+  }
+);
+
+ipcMain.handle(
+  IPC_EVENTS.DOCUMENTS.FETCH,
+  async (
+    _,
+    { id }: DTO.FetchDocumentRequest
+  ): Promise<DTO.FetchDocumentResponse> => {
+    const document = store.get(`documents.${id}`);
+
+    return {
+      data: document,
+    };
+  }
+);
+
+ipcMain.handle(
+  IPC_EVENTS.DOCUMENTS.CREATE,
+  async (): Promise<DTO.CreateDocumentResponse> => {
+    const id = randomUUID();
+    const document: DTO.Document = { id, title: "Untitled" };
+
+    store.set(`documents.${id}`, document);
+
+    return { data: document };
+  }
+);
+
+ipcMain.handle(
+  IPC_EVENTS.DOCUMENTS.SAVE,
+  async (_, document: DTO.SaveDocumentRequest): Promise<void> => {
+    return store.set(`documents.${document.id}`, document);
+  }
+);
+
+ipcMain.handle(
+  IPC_EVENTS.DOCUMENTS.DELETE,
+  async (_, { id }: DTO.DeleteDocumentRequest): Promise<void> => {
+    return store.delete(`documents.${id}`);
   }
 );
--- src/main/store.ts
+++ src/main/store.ts
@@ -1,9 +1,12 @@
 import StorePkg from "electron-store";
+
+import { type Document } from "@shared/types/ipc";
+
 const Store =
   (StorePkg as unknown as { default: typeof StorePkg }).default || StorePkg;
 
 interface StoreProps {
-  documents: Record<string, any>;
+  documents: Record<string, Document>;
 }
 
 export const store = new Store<StoreProps>({
